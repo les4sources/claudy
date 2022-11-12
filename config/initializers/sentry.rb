9712b9170fcbc83c684012b9d831699f344dc42c
Michael Hulet <michael@hulet.eu>
2022-11-12 21:55:47 +0100

    Enable Sentry in production only, no traces sampler

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,13 +1,15 @@
 Sentry.init do |config|
-  config.dsn = Rails.application.credentials[:sentry][:dsn]
+  if Rails.env.production?
+    config.dsn = Rails.application.credentials[:sentry][:dsn]
+  end
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = 1.0
+  # config.traces_sample_rate = 1.0
   # or
   config.traces_sampler = lambda do |context|
-    true
+    false
   end
 end
