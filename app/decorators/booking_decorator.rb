151197f928440321acb6f830e4c2f4b2c30c24ab
Michael Hulet <michael@hulet.eu>
2023-06-14 08:08:45 +0200

    Update booking_decorator.rb

--- app/decorators/booking_decorator.rb
+++ app/decorators/booking_decorator.rb
@@ -129,10 +129,10 @@ class BookingDecorator < ApplicationDecorator
   def options_text
     text = []
     text << "location de la salle" if object.option_partyhall?
-    text << "Pizza Party privée" if @booking.option_pizza_party?
-    text << "pains et viennoiseries" if @booking.option_bread?
-    text << "baby-sitting" if @booking.option_babysitting?
-    text << "initiation au disc-golf" if @booking.option_discgolf?
+    text << "Pizza Party privée" if object.option_pizza_party?
+    text << "pains et viennoiseries" if object.option_bread?
+    text << "baby-sitting" if object.option_babysitting?
+    text << "initiation au disc-golf" if object.option_discgolf?
     text.join(", ")
   end
 
