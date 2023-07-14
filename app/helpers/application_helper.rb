cc91cd191732de87b87bd495cd902c1326c3e962
Michael Hulet <michael@hulet.eu>
2023-07-14 11:37:01 +0200

    Improve calendar UX

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,16 +12,16 @@ module ApplicationHelper
     shared_classes = "text-xs font-semibold text-center px-1 py-0.5 rounded"
     case room.level
     when 0
-      content_tag(:span, room.code, class: "#{shared_classes} bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900")
+      content_tag(:span, room.code, class: "#{shared_classes} bg-indigo-100 text-indigo-800")
     when 1
-      content_tag(:span, room.code, class: "#{shared_classes} bg-purple-100 text-purple-800 dark:bg-purple-200 dark:text-purple-900")
+      content_tag(:span, room.code, class: "#{shared_classes} bg-purple-100 text-purple-800")
     when 2
-      content_tag(:span, room.code, class: "#{shared_classes} bg-pink-100 text-pink-800 dark:bg-pink-200 dark:text-pink-900")
+      content_tag(:span, room.code, class: "#{shared_classes} bg-pink-100 text-pink-800")
     end
   end
 
   def space_badge(space)
-    shared_classes = "text-xs font-semibold text-center py-0.5 rounded"
-    content_tag(:span, space.code, class: "#{shared_classes} bg-indigo-100 text-indigo-800 dark:bg-indigo-200 dark:text-indigo-900")
+    shared_classes = "text-xs font-semibold text-center py-0.5 px-1 rounded"
+    content_tag(:span, space.code, class: "#{shared_classes} bg-indigo-100 text-indigo-800")
   end
 end
