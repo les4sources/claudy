aee89b459628ae085bf23a0ad251dcbcddf51165
xMike <michael@hulet.eu>
2023-10-28 22:50:43 +0200

    Refactor revenue reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,32 +1,19 @@
 class ReportsController < BaseController
   def index
-    @start_date, @end_date = parse_date_range
-    revenue_by_month_for_bookings = 
-      Booking.where(status: "confirmed", from_date: @start_date..@end_date)
-             .group('(EXTRACT(YEAR FROM from_date))::integer')
-             .group('(EXTRACT(MONTH FROM from_date))::integer')
-             .order('2 DESC, 3 DESC')
-             .sum(:price_cents)
-
-    @revenue_by_month_for_bookings = revenue_by_month_for_bookings.transform_values { |v| v / 100.0 }
-    revenue_by_month_for_space_bookings = 
-      SpaceBooking.where(status: "confirmed", from_date: @start_date..@end_date)
-             .group('(EXTRACT(YEAR FROM from_date))::integer')
-             .group('(EXTRACT(MONTH FROM from_date))::integer')
-             .order('2 DESC, 3 DESC')
-             .sum(:price_cents)
-    @revenue_by_month_for_space_bookings = revenue_by_month_for_space_bookings.transform_values { |v| v / 100.0 }
+    @year = params.fetch(:year, Time.now.year.to_s)
+    @revenues_by_month_for_bookings = Booking.where(status: 'confirmed')
+                                .where('strftime("%Y", from_date) = ?', @year)
+                                .group(Arel.sql("strftime('%m', from_date)"))
+                                .order(Arel.sql("strftime('%m', from_date)"))
+                                .sum(:price_cents)
+    @revenues_by_month_for_space_bookings = SpaceBooking.where(status: 'confirmed')
+                                .where('strftime("%Y", from_date) = ?', @year)
+                                .group(Arel.sql("strftime('%m', from_date)"))
+                                .order(Arel.sql("strftime('%m', from_date)"))
+                                .sum(:price_cents)
   end
 
   private
-
-  def parse_date_range
-    if params[:start_date] && params[:end_date]
-      [Date.parse(params[:start_date]), Date.parse(params[:end_date])]
-    else
-      [Date.yesterday.beginning_of_year, Date.today.end_of_year]
-    end
-  end
 
   def set_presenters
     @menu_presenter = Components::MenuPresenter.new(
