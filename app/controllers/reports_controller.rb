f288d6b87a6a4d9a9c0b2fd2dfb5c96748a2a492
xMike <michael@hulet.eu>
2023-10-28 23:14:25 +0200

    Fix reports for PostgreSQL

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,16 +1,18 @@
 class ReportsController < BaseController
   def index
-    @year = params.fetch(:year, Time.now.year.to_s)
-    @revenues_by_month_for_bookings = Booking.where(status: 'confirmed')
-                                .where('strftime("%Y", from_date) = ?', @year)
-                                .group(Arel.sql("strftime('%m', from_date)"))
-                                .order(Arel.sql("strftime('%m', from_date)"))
-                                .sum(:price_cents)
-    @revenues_by_month_for_space_bookings = SpaceBooking.where(status: 'confirmed')
-                                .where('strftime("%Y", from_date) = ?', @year)
-                                .group(Arel.sql("strftime('%m', from_date)"))
-                                .order(Arel.sql("strftime('%m', from_date)"))
-                                .sum(:price_cents)
+    @year = params.fetch(:year, Time.now.year).to_i
+    @revenues_by_month_for_bookings = 
+      Booking.where(status: 'confirmed')
+             .where("EXTRACT(YEAR FROM from_date)::integer = ?", @year)
+             .group(Arel.sql("EXTRACT(MONTH FROM from_date)::integer"))
+             .order(Arel.sql("EXTRACT(MONTH FROM from_date)::integer"))
+             .sum(:price_cents)
+    @revenues_by_month_for_space_bookings = 
+      SpaceBooking.where(status: 'confirmed')
+                  .where("EXTRACT(YEAR FROM from_date)::integer = ?", @year)
+                  .group(Arel.sql("EXTRACT(MONTH FROM from_date)::integer"))
+                  .order(Arel.sql("EXTRACT(MONTH FROM from_date)::integer"))
+                  .sum(:price_cents)
   end
 
   private
