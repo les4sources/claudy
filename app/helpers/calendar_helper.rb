9b75e3619770bc779bb5f6aa80c527ab1157e65f
xMike <michael@hulet.eu>
2023-10-31 15:38:29 +0100

    Improve responsiveness

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,6 @@
 module CalendarHelper
   def button_to_next_month(current_date, data = {}, options = {})
-    link_to(params.permit(:date).merge(date: current_date.next_month), class: "btn-page-header-with-icon", data: data) do
+    link_to(params.permit(:date, :no_title).merge(date: current_date.next_month), class: "btn-page-header-with-icon", data: data) do
       if options[:no_label].nil?
         button_label_with_icon(l(current_date.next_month, format: "%B %Y"), "arrow_small_right", right: true)
       else
@@ -26,7 +26,7 @@ module CalendarHelper
   end
 
   def button_to_previous_month(current_date, data = {}, options = {})
-    link_to(params.permit(:date).merge(date: current_date.prev_month), class: "btn-page-header-with-icon", data: data) do
+    link_to(params.permit(:date, :no_title).merge(date: current_date.prev_month), class: "btn-page-header-with-icon", data: data) do
       if options[:no_label].nil?
         button_label_with_icon(l(current_date.prev_month, format: "%B %Y"), "arrow_small_left")
       else
