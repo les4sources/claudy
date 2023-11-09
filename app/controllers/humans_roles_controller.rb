cc9aea10320595b25cbd0e2e5c2ed9e001495570
xMike <michael@hulet.eu>
2023-11-09 18:17:13 +0100

    Render streams from controller

--- app/controllers/humans_roles_controller.rb
+++ app/controllers/humans_roles_controller.rb
@@ -2,6 +2,10 @@ class HumansRolesController < BaseController
   def create
     @human_role = HumanRole.create(human_role_params)
     @human_roles = HumanRole.where(date: @human_role.date)
+    render turbo_stream: 
+      turbo_stream.replace("human-role-#{@human_role.date.iso8601}-#{@human_role.human_id}-#{@human_role.role_id}",
+                          partial: "humans_roles/human_role",
+                          locals: { human: @human_role.human, role: @human_role.role, date: @human_role.date, human_roles: @human_roles })
   end
 
   def destroy
@@ -11,6 +15,10 @@ class HumansRolesController < BaseController
     if @human_role
       @human_role.destroy
       @human_roles = HumanRole.where(date: date)
+      render turbo_stream: 
+        turbo_stream.replace("human-role-#{@human_role.date.iso8601}-#{@human_role.human_id}-#{@human_role.role_id}",
+                            partial: "humans_roles/human_role",
+                            locals: { human: @human_role.human, role: @human_role.role, date: @human_role.date, human_roles: @human_roles })
     end
   end
 
