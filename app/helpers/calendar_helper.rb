de5e2003b4ceb64ada116a3fc18a488c0cf2d94a
xMike <michael@hulet.eu>
2024-02-06 10:07:18 +0100

    Show "previous/next months" links on small devices

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -10,15 +10,15 @@ module CalendarHelper
   end
 
   def buttons_to_next_months(current_date, data = {})
-    content_tag(:div, class: "hidden md:inline-flex rounded-md shadow-sm", role: "group") do
+    content_tag(:div, class: "inline-flex rounded-md shadow-sm", role: "group") do
       links = []
       links << link_to(params.permit(:date).merge(date: current_date.next_month), class: "btn-group-page-header-with-icon border rounded-l-lg bg-blue-100", data: data) do
         l(current_date.next_month, format: "%B %Y")
       end
-      links << link_to(params.permit(:date).merge(date: current_date + 2.months), class: "btn-group-page-header-with-icon bg-blue-200", data: data) do
+      links << link_to(params.permit(:date).merge(date: current_date + 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
         l(current_date + 2.months, format: "%B %Y")
       end
-      links << link_to(params.permit(:date).merge(date: current_date + 3.months), class: "btn-group-page-header-with-icon border rounded-r-md bg-blue-300", data: data) do
+      links << link_to(params.permit(:date).merge(date: current_date + 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-r-md bg-blue-300", data: data) do
         button_label_with_icon(l(current_date + 3.months, format: "%B %Y"), "arrow_small_right", right: true)
       end
       links.join.html_safe
@@ -36,12 +36,12 @@ module CalendarHelper
   end
 
   def buttons_to_previous_months(current_date, data = {})
-    content_tag(:div, class: "hidden md:inline-flex rounded-md shadow-sm mr-2", role: "group") do
+    content_tag(:div, class: "inline-flex rounded-md shadow-sm mr-2", role: "group") do
       links = []
-      links << link_to(params.permit(:date).merge(date: current_date - 3.months), class: "btn-group-page-header-with-icon border rounded-l-lg bg-blue-300", data: data) do
+      links << link_to(params.permit(:date).merge(date: current_date - 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-l-lg bg-blue-300", data: data) do
         button_label_with_icon(l(current_date - 3.months, format: "%B %Y"), "arrow_small_left")
       end
-      links << link_to(params.permit(:date).merge(date: current_date - 2.months), class: "btn-group-page-header-with-icon bg-blue-200", data: data) do
+      links << link_to(params.permit(:date).merge(date: current_date - 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
         l(current_date - 2.months, format: "%B %Y")
       end
       links << link_to(params.permit(:date).merge(date: current_date.prev_month), class: "btn-group-page-header-with-icon border rounded-r-md bg-blue-100", data: data) do
