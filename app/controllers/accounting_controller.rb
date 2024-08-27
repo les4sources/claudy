c7cdc4158ecbcb11531ccbccbc2c8ba6b540ad14
xMike <michael@hulet.eu>
2024-08-27 09:55:18 +0200

    List bookings with requested invoice

--- app/controllers/accounting_controller.rb
+++ app/controllers/accounting_controller.rb
@@ -4,9 +4,17 @@ class AccountingController < BaseController
       .decorate_collection(
         Booking.where(tier: "non défini", status: ["confirmed", "pending"])
       )
+    @bookings_with_requested_invoice = BookingDecorator
+      .decorate_collection(
+        Booking.where(status: "confirmed", invoice_status: "requested")
+      )
     @space_bookings = SpaceBookingDecorator
       .decorate_collection(
         SpaceBooking.where(tier: "non défini", status: ["confirmed", "pending"])
+      )
+    @space_bookings_with_requested_invoice = SpaceBookingDecorator
+      .decorate_collection(
+        SpaceBooking.where(status: "confirmed", invoice_status: "requested")
       )
   end
 
