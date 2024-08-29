13c9a8a289bf00a04e09f4c2ed13bac2e035dec7
samontour <84902592+samontour@users.noreply.github.com>
2024-08-29 20:09:30 +0200

    fixing issue with payment_requests drop

--- db/migrate/20240816073915_drop_payment_requests.rb
+++ db/migrate/20240816073915_drop_payment_requests.rb
@@ -1,6 +1,11 @@
 class DropPaymentRequests < ActiveRecord::Migration[7.0]
+  
+
   def change
+    execute "ALTER TABLE payments DROP CONSTRAINT fk_rails_d6c292006a CASCADE"
     drop_table :payment_requests_stay_items
     drop_table :payment_requests
   end
+
+ 
 end
