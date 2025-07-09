87636a81deebfcdae12be328e1cf80902137290e
xMike <michael@hulet.eu>
2025-07-09 22:17:09 +0200

    WIP on public stay page

--- app/controllers/public/stays_controller.rb
+++ app/controllers/public/stays_controller.rb
@@ -3,6 +3,7 @@ class Public::StaysController < Public::BaseController
 
   def show
     @stay = Stay.find_by!(token: params[:token]).decorate
+    @stay_items = @stay.stay_items.decorate
     # @reservations_by_date = @stay.reservations.decorate.to_a.group_by { |r| r.date }
   rescue ActiveRecord::RecordNotFound
     raise ActionController::RoutingError.new('Not Found')
