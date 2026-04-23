57a86bfcb0037e9b8fcd1182017c5b00af306272
xMike <michael@hulet.eu>
2026-04-23 12:12:56 +0200

    Update calendar helper to include :view parameter in month navigation links

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,6 +1,6 @@
 module CalendarHelper
   def button_to_next_month(current_date, data = {}, options = {})
-    link_to(params.permit(:date, :no_title).merge(date: current_date.next_month), class: "btn-page-header-with-icon", data: data) do
+    link_to(params.permit(:date, :no_title, :view).merge(date: current_date.next_month), class: "btn-page-header-with-icon", data: data) do
       if options[:no_label].nil?
         button_label_with_icon(l(current_date.next_month, format: "%B %Y"), "arrow_small_right", right: true)
       else
@@ -12,13 +12,13 @@ module CalendarHelper
   def buttons_to_next_months(current_date, data = {})
     content_tag(:div, class: "inline-flex flex-wrap rounded-md shadow-sm", role: "group") do
       links = []
-      links << link_to(params.permit(:date).merge(date: current_date.next_month), class: "btn-group-page-header-with-icon border rounded-l-lg bg-blue-100", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date.next_month), class: "btn-group-page-header-with-icon border rounded-l-lg bg-blue-100", data: data) do
         l(current_date.next_month, format: "%B %Y")
       end
-      links << link_to(params.permit(:date).merge(date: current_date + 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date + 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
         l(current_date + 2.months, format: "%B %Y")
       end
-      links << link_to(params.permit(:date).merge(date: current_date + 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-r-md bg-blue-300", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date + 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-r-md bg-blue-300", data: data) do
         button_label_with_icon(l(current_date + 3.months, format: "%B %Y"), "arrow_small_right", right: true)
       end
       links.join.html_safe
@@ -26,7 +26,7 @@ module CalendarHelper
   end
 
   def button_to_previous_month(current_date, data = {}, options = {})
-    link_to(params.permit(:date, :no_title).merge(date: current_date.prev_month), class: "btn-page-header-with-icon", data: data) do
+    link_to(params.permit(:date, :no_title, :view).merge(date: current_date.prev_month), class: "btn-page-header-with-icon", data: data) do
       if options[:no_label].nil?
         button_label_with_icon(l(current_date.prev_month, format: "%B %Y"), "arrow_small_left")
       else
@@ -38,13 +38,13 @@ module CalendarHelper
   def buttons_to_previous_months(current_date, data = {})
     content_tag(:div, class: "inline-flex flex-wrap rounded-md shadow-sm mr-2", role: "group") do
       links = []
-      links << link_to(params.permit(:date).merge(date: current_date - 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-l-lg bg-blue-300", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date - 3.months), class: "hidden md:inline-flex btn-group-page-header-with-icon border rounded-l-lg bg-blue-300", data: data) do
         button_label_with_icon(l(current_date - 3.months, format: "%B %Y"), "arrow_small_left")
       end
-      links << link_to(params.permit(:date).merge(date: current_date - 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date - 2.months), class: "hidden md:inline-flex btn-group-page-header-with-icon bg-blue-200", data: data) do
         l(current_date - 2.months, format: "%B %Y")
       end
-      links << link_to(params.permit(:date).merge(date: current_date.prev_month), class: "btn-group-page-header-with-icon border rounded-r-md bg-blue-100", data: data) do
+      links << link_to(params.permit(:date, :view).merge(date: current_date.prev_month), class: "btn-group-page-header-with-icon border rounded-r-md bg-blue-100", data: data) do
         l(current_date.prev_month, format: "%B %Y")
       end
       links.join.html_safe
